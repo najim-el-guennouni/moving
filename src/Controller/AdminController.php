089a8f1dbb7b7f746d9e345d3dbdec15f1dac42e
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2022-05-19 11:05:07 +0100

    part 4

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -18,4 +18,39 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/my_profile.html.twig');
     }
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories(): Response
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos(): Response
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+    /**
+     * @Route("/upload_video", name="upload_video")
+     */
+    public function uploadvideo(): Response
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users(): Response
+    {
+        return $this->render('admin/users.html.twig');
+    }
+    /**
+     * @Route("/edit_category", name="edit_category")
+     */
+    public function editcategory(): Response
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
 }
